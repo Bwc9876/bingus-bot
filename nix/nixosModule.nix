0dc090dd9d4c392a0f809e6bd9c101b08ab4b103
Ben C <bwc9876@gmail.com>
2025-04-01 20:16:42 -0400

    Add NixOS Module

---
+++ nix/nixosModule.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.bingus-bot;
+in {
+  options.bingus-bot = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = false;
+      example = true;
+      description = "Whether to enable Bingus, a Discord bot that uses Markov Chains.";
+    };
+
+    brainFile = lib.mkOption {
+      default = "/var/lib/bingus/brain.msgpackz";
+      type = lib.types.path;
+      description = "The path to save Bingus' brain to.";
+    };
+
+    tokenFile = lib.mkOption {
+      default = "/var/lib/bingus/token";
+      type = lib.types.path;
+      description = "The path to load the Discord bot token to auth to the gateway with";
+    };
+
+    replyChannels = lib.mkOption {
+      default = [];
+      type = lib.types.listOf lib.types.number;
+      description = "List of channel IDs that the bot should have a chance to reply in";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.bingus = let
+      replyChannelsStr = lib.strings.concatStrings (lib.strings.intersperse "/" (builtins.map builtins.toString cfg.replyChannels));
+    in {
+      wanted-by = ["multi-user.target"];
+      environment."Markov.REPLY_CHANNELS" = replyChannelsStr;
+      environment."Markov.BRAIN_FILE" = cfg.brainFile;
+      serviceConfig.execStart = ''
+        TOKEN=$(cat ${cfg.tokenFile}) ${pkgs.bingus-env}/bin/bingus
+      '';
+    };
+  };
+}
