a0386e02d53459079fe420f861e95cec4412d448
Ben C <bwc9876@gmail.com>
2025-04-01 20:46:45 -0400

    Fix modules

--- flake.nix
+++ flake.nix
@@ -23,14 +23,68 @@
     };
   };
 
-  outputs = inputs @ { flakelight, ... }:
-    flakelight ./. ({ lib, ... }: {
+  outputs = inputs @ {flakelight, ...}:
+    flakelight ./. ({
+      lib,
+      inputs',
+      ...
+    }: {
       inherit inputs;
       systems = lib.systems.flakeExposed;
       pname = "bingus";
       formatters = {
         "*.nix" = "alejandra .";
         "*.py" = "ruff format .";
+      };
+      nixosModule = {
+        pkgs,
+        lib,
+        config,
+        ...
+      }: let
+        cfg = config.services.bingus-bot;
+      in {
+        options.services.bingus-bot = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            example = true;
+            description = "Whether to enable Bingus, a Discord bot that uses Markov Chains.";
+          };
+
+          brainFile = lib.mkOption {
+            default = "/var/lib/bingus/brain.msgpackz";
+            type = lib.types.path;
+            description = "The path to save Bingus' brain to.";
+          };
+
+          tokenFile = lib.mkOption {
+            default = "/var/lib/bingus/token";
+            type = lib.types.path;
+            description = "The path to load the Discord bot token to auth to the gateway with";
+          };
+
+          replyChannels = lib.mkOption {
+            default = [];
+            type = lib.types.listOf lib.types.number;
+            description = "List of channel IDs that the bot should have a chance to reply in";
+          };
+        };
+
+        config = lib.mkIf cfg.enable {
+          systemd.services.bingus = let
+            replyChannelsStr = lib.strings.concatStrings (lib.strings.intersperse "/" (builtins.map builtins.toString cfg.replyChannels));
+          in {
+            wantedBy = ["multi-user.target"];
+            after = ["network.target"];
+            environment."Markov.REPLY_CHANNELS" = replyChannelsStr;
+            environment."Markov.BRAIN_FILE" = cfg.brainFile;
+            serviceConfig.execStart = ''
+              mkdir -p $(dirname ${cfg.brainFile})
+              TOKEN=$(cat ${cfg.tokenFile}) ${inputs'.packages.bingus-env}/bin/bingus
+            '';
+          };
+        };
       };
     });
 }
