77e4635e89c8d9fffa8f0250a0fcec28d55dbc27
Ben C <ben@bwc9876.dev>
2026-03-12 20:36:14 -0400

    Update README, NixOS Module

--- nix/nixosModule.nix
+++ nix/nixosModule.nix
@@ -29,27 +29,62 @@ in {
 
   config = lib.mkIf cfg.enable {
     systemd.services.bingus = let
-      replyChannelsStr = lib.strings.concatStrings (lib.strings.intersperse "," (builtins.map builtins.toString cfg.replyChannels));
+      replyChannelsStr = lib.strings.concatStrings (
+        lib.strings.intersperse "," (builtins.map builtins.toString cfg.replyChannels)
+      );
     in {
       wantedBy = ["multi-user.target"];
-      after = ["network.target"];
+      after = [
+        "network-online.target"
+      ];
+      wants = [
+        "network-online.target"
+      ];
 
-      environment.REPLY_CHANNELS = replyChannelsStr;
-
-      script = ''
-        export TOKEN=$(cat "$CREDENTIALS_DIRECTORY/BINGUS_BOT_TOKEN")
-        export BRAIN_FILE=$STATE_DIRECTORY/brain.msgpackz
-        ${pkgs.bingus}/bin/bingus
-      '';
+      environment = {
+        REPLY_CHANNELS = replyChannelsStr;
+        TOKEN_FILE = "%d/token";
+        BRAIN_FILE = "brain.msgpackz";
+      };
 
       serviceConfig = {
+        ExecStart = lib.getExe pkgs.bingus;
         Restart = "always";
-        RestartSec = "5s";
-        User = "bingus-bot";
-        Group = "bingus-bot";
         StateDirectory = "bingus";
-        DynamicUser = true;
-        LoadCredential = "BINGUS_BOT_TOKEN:${cfg.tokenFile}";
+        StateDirectoryMode = "0755";
+        LoadCredential = "token:${cfg.tokenFile}";
+
+        # Hardening
+        RemoveIPC = true;
+        CapabilityBoundingSet = ["CAP_NET_BIND_SERVICE"];
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        ProtectClock = true;
+        ProtectKernelLogs = true;
+        ProtectControlGroups = true;
+        ProtectKernelModules = true;
+        PrivateMounts = true;
+        SystemCallArchitectures = ["native"];
+        MemoryDenyWriteExecute = true;
+        RestrictNamespaces = true;
+        RestrictSUIDSGID = true;
+        ProtectHostname = true;
+        LockPersonality = true;
+        ProtectKernelTunables = true;
+        RestrictAddressFamilies = [
+          "AF_UNIX"
+          "AF_INET"
+          "AF_INET6"
+        ];
+        RestrictRealtime = true;
+        DeviceAllow = [""];
+        ProtectSystem = "strict";
+        ProtectProc = "invisible";
+        ProcSubset = "pid";
+        ProtectHome = true;
+        PrivateUsers = true;
+        PrivateTmp = true;
+        UMask = "0077";
       };
     };
   };
